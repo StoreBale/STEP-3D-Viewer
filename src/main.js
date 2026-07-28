aa06ec2480fa3b9db1769402d02f0c41fb4a15b9
StoreBale <71181894+StoreBale@users.noreply.github.com>
2026-07-29 01:46:48 +0800

    Keep quality selector available on mobile

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import './style.css';
+import './mobile.css';
 
 const $ = (selector) => document.querySelector(selector);
 const viewer = $('#viewer');
